ec10305be7be78e0ecc53a4af4726f6900c322a0
HelioGuilherme66 <helioxentric@gmail.com>
2015-10-02 01:11:02 +0100

    Tasks file for invoke adapted from robotframework. Experimental Java packaging for Selenium2Library.

---
+++ src/java/org/robotframework/Selenium2Library.java
@@ -0,0 +1,35 @@
+/* Copyright 2008-2015 Nokia Solutions and Networks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.robotframework;
+
+/**
+ *
+ * Entry point for using Selenium2Library from Java programs.
+ *
+ */
+public class Selenium2Library {
+
+    /**
+     * Entry point when used as a main program.
+     * 
+     * @param args
+     *              The command line options that will just be ignored.
+     */
+    public static void main(String[] args) {
+        System.exit(0);
+    }
+
+}
